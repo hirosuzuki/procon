ae45bf506dffa740683dae54adcb9d0d4801e674
Hiroaki Suzuki <Hiroaki Suzuki>
2018-01-14 02:41:20 +0900

    practice

---
+++ atcoder/practice_1/main.py
@@ -0,0 +1,7 @@
+# https://practice.contest.atcoder.jp/tasks/practice_1
+
+a = int(input())
+b, c = [int(x) for x in input().split()]
+s = input()
+
+print(a + b + c, s)
---
+++ atcoder/practice_2/main.py
@@ -0,0 +1,44 @@
+# https://practice.contest.atcoder.jp/tasks/practice_2
+
+n, q = [int(x) for x in input().split()]
+cs = [chr(ord("A") + i) for i in range(n)]
+
+# import random
+# n = 26
+# vs = [i for i in range(n)]
+# random.shuffle(vs)
+# answer = cs[:]
+# random.shuffle(answer)
+# print(answer)
+
+cmp_counter = 0
+
+def cmp(x, y):
+    global cmp_counter
+    cmp_counter += 1
+    # return ord(x) - ord(y)
+    # r = answer.index(x) - answer.index(y)
+    # print(x, y, r)
+    # return r
+    print("?", x, y, flush=True)
+    resp = input()
+    return 1 if resp == ">" else -1
+
+def sort(x):
+    result = []
+    for c in cs:
+        l = 0
+        r = len(result)
+        while l < r:
+            m = (l + r) // 2
+            if cmp(c, result[m]) > 0:
+                l = m + 1
+            else:
+                r = m
+        result.insert(l, c)
+    return result
+
+cs = sort(cs)
+
+print("!", "".join(cs))
+# print(cmp_counter)
