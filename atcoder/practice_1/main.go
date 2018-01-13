ce39d1e0c2e479131571fe81c4d3184ce84911ae
Hiroaki Suzuki <Hiroaki Suzuki>
2018-01-14 02:59:10 +0900

    見やすくリファクタリング

--- atcoder/practice_1/main.go
+++ atcoder/practice_1/main.go
@@ -1,15 +1,15 @@
 package main
 
+// https://practice.contest.atcoder.jp/tasks/practice_1
+
 import "fmt"
 
 func main() {
 	var a int64
-	var b int64
-	var c int64
+	var b, c int64
 	var s string
 	fmt.Scan(&a)
-	fmt.Scan(&b)
-	fmt.Scan(&c)
+	fmt.Scan(&b, &c)
 	fmt.Scan(&s)
 	fmt.Println(a+b+c, s)
 }
