a199f595ea15a854d6d88af2c80add90c39638c4
Hiroaki Suzuki <Hiroaki Suzuki>
2018-03-04 23:29:01 +0900

    ABC090 リファクタ

--- atcoder/abc089/c.py
+++ atcoder/abc089/c.py
@@ -1,17 +1,9 @@
-from collections import defaultdict
+from collections import Counter
 from itertools import combinations
 
 N = int(input())
 S = [input() for i in range(N)]
 
-CHARS = "MARCH"
+xs = Counter(s[0] for s in S)
 
-xs = defaultdict(int)
-
-for s in S:
-    xs[s[0]] += 1
-
-def calc(xs):
-    return sum(xs[a] * xs[b] * xs[c] for a, b, c in combinations(CHARS, 3))
-
-print(calc(xs))
+print(sum(xs[a] * xs[b] * xs[c] for a, b, c in combinations("MARCH", 3)))
--- atcoder/abc089/d.py
+++ atcoder/abc089/d.py
@@ -1,6 +1,6 @@
 H, W, D = [int(_) for _ in input().split()]
 A = [[int(_) for _ in input().split()] for i in range(H)]
-Q, = [int(_) for _ in input().split()]
+Q = int(input())
 LR = [[int(_) for _ in input().split()] for i in range(Q)]
 
 pos = {}
@@ -8,13 +8,10 @@ for i, row in enumerate(A):
     for j, x in enumerate(row):
         pos[x] = (i, j)
 
-from collections import defaultdict
-
-dt = defaultdict(int)
+dt = [0] * (H * W + 1)
 for x in range(D + 1, H * W + 1):
     y = x - D
-    p1 = pos[x]
-    p2 = pos[y]
+    p1, p2 = pos[x], pos[y]
     dt[x] = dt[y] + abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 for l, r in LR:
