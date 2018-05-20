36e5e636ade09de2eaad629aae4c3694cd187ac2
Hiroaki Suzuki <Hiroaki Suzuki>
2018-05-20 20:51:38 +0900

    abs

--- atcoder/abs/arc065_a.py
+++ atcoder/abs/arc065_a.py
@@ -1,8 +1,19 @@
 import re
 
-def solve(S):
+def solve0(S):
     r = re.compile(r'^(dreamer|eraser|dream|erase)*$')
     return r.match(S)
+
+def solve(S):
+    words = "dreamer", "eraser", "dream", "erase"
+    while S:
+        for word in words:
+            if S.endswith(word):
+                S = S[:-len(word)]
+                break
+        else:
+            return False
+    return True
 
 S = input()
 
