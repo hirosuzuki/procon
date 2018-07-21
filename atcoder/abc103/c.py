222a67a826bd07def99020472bd66cfde3d70600
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2018-07-21 23:26:33 +0900

    ABC103

--- atcoder/abc103/c.py
+++ atcoder/abc103/c.py
@@ -1,21 +1,3 @@
 N = int(input())
 A = [int(_) for _ in input().split()]
-
-def gcd(a, b):
-    if a < b:
-        a, b = b, a
-    while b:
-        a, b = b, a % b
-    return a
-
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-#l = 1
-#for a in A:
-#    l = lcm(l, a)
-
-#r = sum((l - 1) % a for a in A)
-r = sum(a - 1 for a in A)
-
-print(r)
+print(sum(A) - N)
--- atcoder/abc103/d.py
+++ atcoder/abc103/d.py
@@ -7,14 +7,10 @@ result = 1
 
 x1, x2 = 1, N
 for a, b in AB:
-    # print(x1, x2, a, b)
     if x2 <= a:
-        # print("*")
         result += 1
         x1, x2 = a, b
     else:
-        x1 = max(x1, a)
-        x2 = min(x2, b)
+        x1, x2 = a, min(x2, b)
 
 print(result)
-
