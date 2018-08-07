67b59f1f2f5966f99612f9350560b845e240ab67
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2018-08-08 01:16:42 +0900

    ABC104復習

--- atcoder/abc104/c.py
+++ atcoder/abc104/c.py
@@ -1,36 +1,32 @@
 D, G = [int(_) for _ in input().split()]
 PC = [tuple(int(_) for _ in input().split()) for i in range(D)]
+P = [_[0] for _ in PC]
+C = [_[1] for _ in PC]
 
 from itertools import product
+from math import ceil
 
-def calc(D, G, PC, es):
-    r = 0
-    k = None
-    result = 0
-    for i, e, pc in zip(range(D), es, PC):
+INF = 10**20
+
+def calc(pat):
+    solved_promlems = 0
+    total_score = 0
+    left = None
+    for e, score, problem_nums, complete_score in zip(pat, range(100, (D+1)*100, 100), P, C):
         if e:
-            r += (i+1) * 100 * pc[0] + pc[1]
-            result += pc[0]
+            total_score += score * problem_nums + complete_score
+            solved_promlems += problem_nums
         else:
-            k = i
-    if r >= G:
-        return result, r
-    if not k is None:
-        d = (k+1)*100
-        p = PC[k][0]
-        if r + d * (p - 1) >= G:
-            n = (G - r + d - 1) // d
-            result += n
-            r += n * d
-            return result, r
-    return None, None
+            left = score, problem_nums
+    if total_score >= G:
+        return solved_promlems
+    if left:
+        score, problem_nums = left
+        n = ceil((G - total_score) / score)
+        if 1 <= n < problem_nums:
+            return solved_promlems + n
+    return INF
 
-result = 10**20
-
-for es in product((0, 1), repeat=D):
-    r, score = calc(D, G, PC, es)
-    #print(D, G, PC, es, r, score)
-    if not r is None:
-        result = min(result, r)
+result = min(calc(pat) for pat in product((0, 1), repeat=D))
 
 print(result)
