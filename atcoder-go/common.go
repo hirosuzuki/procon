f124ead14412eee4ff46a6d014c368a44eae48ab
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2018-09-13 01:09:58 +0900

    Go Common min, loadints

--- atcoder-go/common.go
+++ atcoder-go/common.go
@@ -1,5 +1,11 @@
 package common
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
 func gcd(a, b int) int {
 	for b != 0 {
 		a, b = b, a%b
@@ -12,4 +18,22 @@ func abs(a int) int {
 		return a
 	}
 	return -a
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func loadints(n int) []int {
+	xs := make([]int, n)
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	for i := 0; i < n; i++ {
+		sc.Scan()
+		xs[i], _ = strconv.Atoi(sc.Text())
+	}
+	return xs
 }
