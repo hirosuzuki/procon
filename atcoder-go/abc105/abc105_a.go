87bb1d3ee97976e11d0559c300dcbd00740d8c87
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2018-09-29 22:05:19 +0900

    ABC105 with Go

---
+++ atcoder-go/abc105/abc105_a.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc *bufio.Scanner
+
+func init() {
+	sc = bufio.NewScanner(os.Stdin)
+	buffsize := 1000000
+	buff := make([]byte, buffsize)
+	sc.Buffer(buff, buffsize)
+	sc.Split(bufio.ScanWords)
+}
+
+func loadint() (result int) {
+	sc.Scan()
+	result, _ = strconv.Atoi(sc.Text())
+	return
+}
+
+func main() {
+	n := loadint()
+	k := loadint()
+	if n%k == 0 {
+		fmt.Println(0)
+	} else {
+		fmt.Println(1)
+	}
+}
