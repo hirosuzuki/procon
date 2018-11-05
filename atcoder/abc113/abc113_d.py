2f0ca4be7927179cdfef2740e55e83cfa3bcf472
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2018-11-06 01:03:33 +0900

    ABC113 D リファクタ

--- atcoder/abc113/abc113_d.py
+++ atcoder/abc113/abc113_d.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 H, W, K = [int(_) for _ in input().split()]
 
 MOD = 1000000007
@@ -7,9 +9,11 @@ pats = [[0]]
 for i in range(1, W):
     pats = [p + [i] for p in pats] + [p[:-1] + [i, i - 1] for p in pats if p[-1] == i - 1]
 
+iws = [Counter(r) for r in zip(*pats)]
+
 rs = [1] + [0] * (W - 1)
 
 for j in range(H):
-    rs = [sum(rs[p[i]] for p in pats) % MOD for i in range(W)]
+    rs = [sum(rs[i] * iw[i] for i in iw) % MOD for iw in iws]
 
 print(rs[K - 1])
