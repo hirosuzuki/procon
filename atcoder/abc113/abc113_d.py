a258f852159b489831b7004176a6a94738a03a57
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2018-11-05 23:48:42 +0900

    ABC113 D リファクタ

--- atcoder/abc113/abc113_d.py
+++ atcoder/abc113/abc113_d.py
@@ -2,22 +2,14 @@ H, W, K = [int(_) for _ in input().split()]
 
 MOD = 1000000007
 
-pats = [""]
+pats = [[0]]
 
-for i in range(W - 1):
-    pats = [p + "0" for p in pats] + [p + "1" for p in pats if not p.endswith("1")]
+for i in range(1, W):
+    pats = [p + [i] for p in pats] + [p[:-1] + [i, i - 1] for p in pats if p[-1] == i - 1]
 
 rs = [1] + [0] * (W - 1)
 
 for j in range(H):
-    nrs = [0] * W
-    for p in pats:
-        drs = rs[:]
-        for i, c in enumerate(p):
-            if c == "1":
-                drs[i], drs[i + 1] = drs[i + 1], drs[i]
-        for i in range(W):
-            nrs[i] = (nrs[i] + drs[i]) 
-    rs = nrs
+    rs = [sum(rs[p[i]] for p in pats) % MOD for i in range(W)]
 
-print(rs[K - 1] % MOD)
+print(rs[K - 1])
