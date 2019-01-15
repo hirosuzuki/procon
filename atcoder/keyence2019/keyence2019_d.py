0987a155e305a11e6e24c2da377ddcedd25301d0
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2019-01-16 00:25:09 +0900

    KEYENCE 2019 D 途中

---
+++ atcoder/keyence2019/keyence2019_d.py
@@ -0,0 +1,8 @@
+N, M = [int(_) for _ in input().split()]
+A = [int(_) for _ in input().split()]
+B = [int(_) for _ in input().split()]
+
+def solve(N, M, A, B):
+    return 0
+
+print(solve(N, M, A, B))
