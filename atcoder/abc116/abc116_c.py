a3ba6211e3cd04df3cd78cf2f38b2c41ac826b57
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2019-01-21 22:15:14 +0900

    ABC116 C

---
+++ atcoder/abc116/abc116_c.py
@@ -0,0 +1,11 @@
+N = int(input())
+H = [int(_) for _ in input().split()]
+
+result = 0
+for i in range(1, 101):
+    for j in range(N):
+        if H[j] >= i:
+            if j == 0 or H[j - 1] < i:
+                result += 1
+
+print(result)
