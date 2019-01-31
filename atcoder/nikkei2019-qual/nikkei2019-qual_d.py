202302fdcd40ff201c7a6c73f85249f8450d6a1b
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2019-02-01 00:12:19 +0900

    Nikkei2019 Qual D 非再帰化

--- atcoder/nikkei2019-qual/nikkei2019-qual_d.py
+++ atcoder/nikkei2019-qual/nikkei2019-qual_d.py
@@ -1,50 +1,46 @@
 N, M = [int(_) for _ in input().split()]
 AB = [[int(_) for _ in input().split()] for i in range(N - 1 + M)]
 
-import sys
-sys.setrecursionlimit(200000)
-
 from collections import defaultdict
 
-vs = defaultdict(set)
-ws = defaultdict(set)
+nodes = list(range(1, N + 1))
+edges = defaultdict(set)
+rev_edges = defaultdict(set)
 
 for a, b in AB:
-    vs[a].add(b)
-    ws[b].add(a)
+    edges[a].add(b)
+    rev_edges[b].add(a)
 
 #print(vs)
 #print(ws)
 
-L = []
-
-cs = [0] * (1 + N)
-
-def visit(n):
-    if cs[n] == 1:
-        return
-    elif cs[n] == 0:
-        cs[n] = 1
-        for m in vs[n]:
-            visit(m)
-        cs[n] = 2
+def TopologicalSort(nodes, edges, rev_edges):
+    '''
+    nodes     ... Node[]
+    edges     ... Dict[From Node](To Node)
+    rev_edges ... Dict[To Node](From Node)
+    '''
+    rev_edges_num = dict((n, len(rev_edges[n])) for n in nodes)
+    L = []
+    S = [n for n in nodes if not rev_edges_num[n]]
+    while S:
+        n = S.pop()
         L.append(n)
+        for m in edges[n]:
+            rev_edges_num[m] -= 1
+            if not rev_edges_num[m]:
+                S.append(m)
+    
+    # プロコン用なのでエラーチェックはしない
 
-for n in range(1, N + 1):
-    if cs[n] == 0:
-        visit(n)
+    return L
 
-#print(L)
+L = TopologicalSort(nodes, edges, rev_edges)
 
-nrank = {}
-
-for r, n in enumerate(L):
-    nrank[n] = r
-
-#print(nrank)
+nrank = dict((n, r) for r, n in enumerate(L))
 
 for n in range(1, N + 1):
     r = 0
-    if ws[n]:
-        r = sorted(ws[n], key=lambda x:nrank[x])[0]
+    if rev_edges[n]:
+        r = sorted(rev_edges[n], key=lambda x:nrank[x], reverse=True)[0]
     print(r)
