b9138137f0f5b6dfa4e9a96663628cd02741d485
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2019-03-10 00:41:05 +0900

    ABC121 B Refactor

--- atcoder/abc121/abc121_b.py
+++ atcoder/abc121/abc121_b.py
@@ -2,12 +2,9 @@ N, M, C = [int(_) for _ in input().split()]
 B = [int(_) for _ in input().split()]
 A = [[int(_) for _ in input().split()] for i in range(N)]
 
-result = 0
-
-for row in A:
-    t = C + sum([x * y for x, y in zip(row, B)])
-    if t > 0:
-        result += 1
+result = sum(
+    C + sum([x * y for x, y in zip(a, B)]) > 0
+    for a in A
+)
 
 print(result)
-
