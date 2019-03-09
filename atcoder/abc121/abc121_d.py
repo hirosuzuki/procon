e2cc87ea4883b7a6c364e2eb764425af6ba12510
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2019-03-10 00:41:20 +0900

    ABC121 D Refactor

--- atcoder/abc121/abc121_d.py
+++ atcoder/abc121/abc121_d.py
@@ -1,33 +1,13 @@
 A, B = [int(_) for _ in input().split()]
 
-"""
-r = 0
-for i in range(17):
-    r ^= i
-    print(i, ("0000" + bin(r)[2:])[-5:])
-"""
+def f(a, b):
+    if a > 0:
+        return f(0, a - 1) ^ f(0, b)
+    if b % 2 == 1:
+        return (b + 1) // 2 % 2
+    else:
+        return b ^ f(0, b - 1)
 
-def calc(n):
-    rs = []
-    rs.append((n + 1) // 2 % 2)
-    for i in range(1, 41):
-        d = 1 << i
-        x = n // d
-        if x % 2 == 0:
-            r = 0
-        else:
-            r = n % 2 ^ 1
-        rs.append(r)
-    return rs
-
-b1 = calc(A - 1)
-b2 = calc(B)
-
-#print(b1)
-#print(b2)
-
-result = 0
-for i in range(41):
-    result |= (b1[i] ^ b2[i]) << i
+result = f(A, B)
 
 print(result)
