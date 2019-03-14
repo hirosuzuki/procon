dc7d53e084c301cf34a16e46bd2734bfdc8525e2
hiro.suzuki <hirosuzuki@users.noreply.github.com>
2019-03-15 01:18:32 +0900

    ABC045 B

--- atcoder/abc/abc045_b.py
+++ atcoder/abc/abc045_b.py
@@ -1,13 +1,7 @@
-S = {}
-S["a"] = list(input())
-S["b"] = list(input())
-S["c"] = list(input())
+S = {c:list(input()) for c in "abc"}
 
 k = "a"
-while 1:
-    if len(S[k]) == 0:
-        break
+while len(S[k]):
     k = S[k].pop(0)
-    # print(S, k)
 
 print(k.upper())
